2bc7be91e584378e0895e39b9ae16653d5ba2c7f
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-09-29 14:20:58 +0200

    Generated Models

---
+++ app/src/main/java/nl/hs_hague/urbangame/model/Checkpoint.java
@@ -0,0 +1,44 @@
+package nl.hs_hague.urbangame.model;
+
+/**
+ * Created by vural on 29.09.16.
+ */
+
+public class Checkpoint {
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private String hint;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getHint() {
+        return hint;
+    }
+
+    public void setHint(String hint) {
+        this.hint = hint;
+    }
+}
--- app/src/main/java/nl/hs_hague/urbangame/model/Room.java
+++ app/src/main/java/nl/hs_hague/urbangame/model/Room.java
@@ -1,6 +1,8 @@
 package nl.hs_hague.urbangame.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by vural on 26.09.16.
@@ -8,6 +10,10 @@ import java.io.Serializable;
 
 public class Room implements Serializable {
     private String name;
+    private List<User> members;
+    private Date startDate;
+    private Date endDate;
+    private List<Checkpoint> checkpoints;
 
     public Room(String name){
         this.name = name;
@@ -19,5 +25,37 @@ public class Room implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Checkpoint> getCheckpoints() {
+        return checkpoints;
+    }
+
+    public void setCheckpoints(List<Checkpoint> checkpoints) {
+        this.checkpoints = checkpoints;
     }
 }
