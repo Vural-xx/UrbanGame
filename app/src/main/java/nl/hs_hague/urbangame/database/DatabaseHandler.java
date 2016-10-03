7843ee82a94ff03380e1b4b41f38c9bfa7878382
Vural Yilmaz <vyilmaz@stud.hs-bremen.de>
2016-10-03 23:14:00 +0200

    Firebase to create Room implemented

---
+++ app/src/main/java/nl/hs_hague/urbangame/database/DatabaseHandler.java
@@ -0,0 +1,33 @@
+package nl.hs_hague.urbangame.database;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import nl.hs_hague.urbangame.model.Room;
+
+/**
+ * Created by vural on 03.10.16.
+ */
+
+public class DatabaseHandler {
+    private DatabaseReference root;
+
+    public DatabaseHandler(){
+        root = FirebaseDatabase.getInstance().getReferenceFromUrl("https://urbangame-4f82c.firebaseio.com/").getRoot();
+    }
+
+    public void createRoom(Room room){
+        Map<String,Object> map = new HashMap<String, Object>();
+        map.put(room.getName(),room);
+        root.child("rooms").updateChildren(map);
+    }
+
+
+
+    public DatabaseReference getRoot(){
+        return root;
+    }
+}
