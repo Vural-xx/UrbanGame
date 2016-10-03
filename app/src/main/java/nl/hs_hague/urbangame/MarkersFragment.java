5c4019ca3a4e6aa6ed615a99b90da793fd3f586b
AlexeyMarcos <admc_86@hotmail.com>
2016-10-03 22:50:28 +0200

    FragmentMarkers

---
+++ app/src/main/java/nl/hs_hague/urbangame/MarkersFragment.java
@@ -0,0 +1,68 @@
+package nl.hs_hague.urbangame;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+public class MarkersFragment extends DialogFragment {
+    String name;
+    EditText name_text;
+
+    public interface MakersFragmentListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+    }
+
+    MakersFragmentListener mListener;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        final LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View inflator = inflater.inflate(R.layout.layout_markers_fragment, null);
+        name_text = (EditText) inflator.findViewById(R.id.name_marker);
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.layout_markers_fragment, null))
+                // Add action buttons
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        name = name_text.getText().toString();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MarkersFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (MakersFragmentListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+}
