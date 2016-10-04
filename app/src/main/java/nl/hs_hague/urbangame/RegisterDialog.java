87a4f31c04bce850ac481a01c076a7d3d75475f3
jolbrys <jakub.olbrys@gmail.com>
2016-10-04 02:19:39 +0200

    added login activity
    Login activity made with the android studio template with register
    dialog

---
+++ app/src/main/java/nl/hs_hague/urbangame/RegisterDialog.java
@@ -0,0 +1,37 @@
+package nl.hs_hague.urbangame;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class RegisterDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View convertView = inflater.inflate(R.layout.dialog_register, null);
+
+        builder.setView(convertView).setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        getDialog().cancel();
+
+                    }
+                });
+
+
+
+        return builder.create();
+    }
+}
