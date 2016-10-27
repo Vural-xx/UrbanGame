13746291c1b6de3499efd88bcb571c638846f8b5
AlexeyMarcos <admc_86@hotmail.com>
2016-10-27 20:54:24 +0200

    Asking for permissions

--- app/src/main/java/nl/hs_hague/urbangame/Welcome_Screen.java
+++ app/src/main/java/nl/hs_hague/urbangame/Welcome_Screen.java
@@ -2,29 +2,47 @@ package nl.hs_hague.urbangame;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 public class Welcome_Screen extends Activity {
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    private GoogleApiClient client;
 
+    private final int MY_PERMISSIONS_REQUEST = 1;
+    private Boolean login, login_face,login_fire, authomatedLogin ;
+    static final Integer GPS_SETTINGS = 7;
+    private GoogleApiClient client;
+    private LocationRequest mLocationRequest;
+    private PendingResult<LocationSettingsResult> result;
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
         Window window = getWindow();
@@ -40,7 +58,7 @@ public class Welcome_Screen extends Activity {
         StartAnimations();
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).addApi(LocationServices.API).build();
     }
 
     private void StartAnimations() {
@@ -67,25 +85,43 @@ public class Welcome_Screen extends Activity {
                         waited += 100;
                     }
                     SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    Boolean login=preferences.getBoolean("Login",false);
-                    Boolean login_face=preferences.getBoolean("Login_face",false);
-                    Boolean login_fire=preferences.getBoolean("Login_fire",false);
-                    Boolean authomatedLogin = preferences.getBoolean(SettingsActivity.authomatic_login_key,true);
-                    if (!login || !authomatedLogin)
-                    {
-                    Intent intent = new Intent(Welcome_Screen.this,
-                            Login.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(intent);
-                    Welcome_Screen.this.finish();
+                    login=preferences.getBoolean("Login",false);
+                    login_face=preferences.getBoolean("Login_face",false);
+                    login_fire=preferences.getBoolean("Login_fire",false);
+                    authomatedLogin = preferences.getBoolean(SettingsActivity.authomatic_login_key,true);
+                    if(ContextCompat.checkSelfPermission(Welcome_Screen.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(Welcome_Screen.this,
+                                Manifest.permission.ACCESS_FINE_LOCATION)) {
+                                /*Permission denied before*/
+                            ActivityCompat.requestPermissions(Welcome_Screen.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST);
+
+                        } else {
+
+                            // No explanation needed, we can request the permission.
+
+                            ActivityCompat.requestPermissions(Welcome_Screen.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST);
+
+                            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                            // app-defined int constant. The callback method gets the
+                            // result of the request.
+                        }
                     }
-                    else
-                    {
-                        Intent intent = new Intent(Welcome_Screen.this,
-                               RoomListActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                        startActivity(intent);
-                        Welcome_Screen.this.finish();
+                    else{
+                        if (!login || !authomatedLogin) {
+                            Intent intent = new Intent(Welcome_Screen.this,
+                                    Login.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                            startActivity(intent);
+                            Welcome_Screen.this.finish();
+                        } else {
+                            Intent intent = new Intent(Welcome_Screen.this,
+                                    RoomListActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                            startActivity(intent);
+                            Welcome_Screen.this.finish();
+                        }
                     }
 
                 } catch (InterruptedException e) {
@@ -97,6 +133,7 @@ public class Welcome_Screen extends Activity {
             }
         };
         splashTread.start();
+
 
     }
 
@@ -134,5 +171,47 @@ public class Welcome_Screen extends Activity {
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                case MY_PERMISSIONS_REQUEST: {
+                    askForGPS();
+                    break;
+                }
+
+            }
+        }
+    }
+    private void askForGPS(){
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(30 * 1000);
+        mLocationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+        result = LocationServices.SettingsApi.checkLocationSettings(client, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(Welcome_Screen.this, GPS_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
     }
 }
